740145b1737468ab9c3b1b5318be801a7de38fe6
Enock <enock.mokua@student.moringaschool.com>
2022-12-26 00:28:34 +0300

    fornat code in landing page

--- src/Pages/landing.js
+++ src/Pages/landing.js
@@ -1,21 +1,21 @@
-import './landing.css'
-import {useState} from 'react'
+import "./landing.css";
+import { useState } from "react";
 
 export default function Landing() {
-  let [email, setEmail] = useState('')
-  let [username, setUsername] = useState('')
-  let [total, setTotal] = useState('')
-  let [phone, setPhone] = useState('')
-  let [numberA, setNumberA] = useState('')
-  let [numberB, setNumberB] = useState('')
-  let [errors, setErrors] = useState([])
+  let [email, setEmail] = useState("");
+  let [username, setUsername] = useState("");
+  let [total, setTotal] = useState("");
+  let [phone, setPhone] = useState("");
+  let [numberA, setNumberA] = useState("");
+  let [numberB, setNumberB] = useState("");
+  let [errors, setErrors] = useState([]);
 
   function handleSubmit(e) {
-    e.preventDefault()
-    fetch('https://pesaapi-production.up.railway.app/users', {
-      method: 'POST',
+    e.preventDefault();
+    fetch("https://pesaapi-production.up.railway.app/users", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         email,
@@ -23,107 +23,107 @@ export default function Landing() {
         total,
         phone,
       }),
-    }).then(r => {
+    }).then((r) => {
       if (r.ok) {
-        r.json().then(err => {
-          setErrors(err.errors)
-          console.log('success', err)
-          setEmail('')
-          setUsername('')
-          setTotal('')
-          setPhone('')
-        })
+        r.json().then((err) => {
+          setErrors(err.errors);
+          console.log("success", err);
+          setEmail("");
+          setUsername("");
+          setTotal("");
+          setPhone("");
+        });
       } else {
-        r.json().then(err => {
-          console.log(err)
-        })
-        console.log(errors)
+        r.json().then((err) => {
+          console.log(err);
+        });
+        console.log(errors);
       }
-    })
+    });
   }
 
   function handleAdd(e) {
-    e.preventDefault()
-    fetch('http://127.0.0.1:3000/numbers', {
-      method: 'POST',
+    e.preventDefault();
+    fetch("http://127.0.0.1:3000/numbers", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         numberA,
         numberB,
       }),
-    }).then(r => {
+    }).then((r) => {
       if (r.ok) {
-        r.json().then(err => {
-          setErrors(err.errors)
-          console.log('success', err)
-          setNumberA('')
-          setNumberB('')
-        })
+        r.json().then((err) => {
+          setErrors(err.errors);
+          console.log("success", err);
+          setNumberA("");
+          setNumberB("");
+        });
       } else {
-        r.json().then(err => {
-          console.log(err)
-        })
-        console.log(errors)
+        r.json().then((err) => {
+          console.log(err);
+        });
+        console.log(errors);
       }
-    })
+    });
   }
 
   function handlePay() {
-    const date = new Date()
-    const year = date.getFullYear()
-    const month = date.getMonth() + 1 // Returns a zero-based month (0-11)
-    const day = date.getDate()
-    const hour = date.getHours()
-    const minutes = date.getMinutes()
-    const seconds = date.getSeconds()
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // Returns a zero-based month (0-11)
+    const day = date.getDate();
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
 
     function secss(x) {
       if (x < 10) {
-        return `0${x}`
+        return `0${x}`;
       } else {
-        return `${x}`
+        return `${x}`;
       }
     }
-    let timestamped = `${year}${month}${day}${hour}${minutes}${secss(seconds)}`
+    let timestamped = `${year}${month}${day}${hour}${minutes}${secss(seconds)}`;
 
-    let headers = new Headers()
-    headers.append('Content-Type', 'application/json')
-    headers.append('Authorization', 'Bearer GNAeMfYCpa3hZ0XLOC6RFEJHzaWf')
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Authorization", "Bearer GNAeMfYCpa3hZ0XLOC6RFEJHzaWf");
 
-    fetch('https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest', {
-      method: 'POST',
+    fetch("https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest", {
+      method: "POST",
       headers,
       body: JSON.stringify({
         BusinessShortCode: 174379,
         Password:
-          'MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjIxMjI1MjAzODA3',
+          "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjIxMjI1MjAzODA3",
         Timestamp: timestamped,
-        TransactionType: 'CustomerPayBillOnline',
+        TransactionType: "CustomerPayBillOnline",
         Amount: 1,
         PartyA: 254717594017,
         PartyB: 174379,
         PhoneNumber: 254717594017,
-        CallBackURL: 'https://mydomain.com/path',
-        AccountReference: 'CompanyXLTD',
-        TransactionDesc: 'Payment of X',
+        CallBackURL: "https://mpesa-jcyc.vercel.app",
+        AccountReference: "New Life",
+        TransactionDesc: "Pharmacy goods",
       }),
     })
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log(error))
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log(error));
   }
 
   function getJob(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     fetch(
-      'https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=d0291057&app_key=e45310af6518f33ea0f2617638ff1d7f',
+      "https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=d0291057&app_key=e45310af6518f33ea0f2617638ff1d7f"
     )
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error(error))
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
   }
 
   return (
@@ -134,28 +134,28 @@ export default function Landing() {
           type="email"
           placeholder="Email"
           value={email}
-          onChange={e => setEmail(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <input
           type="text"
           placeholder="Username"
           value={username}
-          onChange={e => setUsername(e.target.value)}
+          onChange={(e) => setUsername(e.target.value)}
         />
 
         <input
           type="number"
           placeholder="Total Amount"
           value={total}
-          onChange={e => setTotal(e.target.value)}
+          onChange={(e) => setTotal(e.target.value)}
         />
 
         <input
           type="number"
           placeholder="Phone number"
           value={phone}
-          onChange={e => setPhone(e.target.value)}
+          onChange={(e) => setPhone(e.target.value)}
         />
 
         <button type="submit">Submit</button>
@@ -167,18 +167,18 @@ export default function Landing() {
           type="number"
           placeholder="Number 1"
           value={numberA}
-          onChange={e => setNumberA(e.target.value)}
+          onChange={(e) => setNumberA(e.target.value)}
         />
         <input
           type="number"
           placeholder="Number 2"
           value={numberB}
-          onChange={e => setNumberB(e.target.value)}
+          onChange={(e) => setNumberB(e.target.value)}
         />
         <button type="submit">Submit</button>
       </form>
 
       <button onClick={handlePay}>Click Me</button>
     </main>
-  )
+  );
 }
